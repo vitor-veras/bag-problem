6270b3cb098b44980ff507d7be9a8d04045fa2d6
Lucas Pereira Lima <lucas.plima.ti@gmail.com>
2018-03-27 13:51:26 -0300

    PrintBags added.

--- hc/__init__.py
+++ hc/__init__.py
@@ -1,14 +1,15 @@
 
-from heuristic import Heuristic
-from neighbor import Neighbor
-from utils import Generate
-from hill_climbing import HillClimbing
+from hc.heuristic import Heuristic
+from hc.neighbor import Neighbor
+from hc.utils import Generate, Utils
+from hc.hill_climbing import HillClimbing
+
 
 def main():
     #initialState = [[29, 23], [[1, 1], [19, 2], [17, 2], [13, 0], [19, 1]]]
 
-    initialState = Generate(nBag=3, nItem=10).initState()
-    print "Bags: ", initialState[0], " | Wieghts: ", initialState[1]
+    initialState = Generate(nBag=3, nItem=30).initState()
+    print("Bags: ", initialState[0], " | Weights: ", initialState[1])
 
     #h = Heuristic(initialState)
     #n = Neighbor(initialState).generateState2()
@@ -28,10 +29,13 @@ def main():
     #print "heuristica %s " % ws
     #h.listItems()
 
-    print "----------------------------"
-
+    print ("----------------------------")
+    ut = Utils(final)
     #print initialState[1]
-    print "Final Bags: ", final[0], " | Wieghts: ", final[1]
+    #print ("Final Bags: ", final[0], " | Weights: ", final[1])
+    ut.printBags()
 
 
-main()
+
+if __name__ == '__main__':
+    main()
--- hc/utils.py
+++ hc/utils.py
@@ -5,6 +5,7 @@
 import random
 import copy
 
+
 class Generate(object):
     def __init__(self, vItem=(1, 3, 5, 7, 11, 13, 17, 19),
                  vBag = (23, 29, 31, 37, 41, 43, 47),
@@ -19,13 +20,15 @@ class Generate(object):
         items = []
         bags = []
 
+        aux = list(self.vBag)
         for i in range(self.nBag):
-            bags.append(self.vBag[random.randint(0,len(self.vBag) - 1)])
+            vol = aux[random.randint(0, len(self.vBag) - 1)]
+            bags.append(vol)
+            aux.remove(vol)
         for i in range(self.nItem):
-            items.append([self.vItem[random.randint(0,len(self.vItem) - 1)], self.nBag])
+            items.append([self.vItem[random.randint(0, len(self.vItem) - 1)], self.nBag])
 
         return [bags, items]
-
 
     def desalloc(self, state):
         newState = copy.deepcopy(state)
@@ -34,3 +37,32 @@ class Generate(object):
             newState[1][i][1] = len(newState[0])
 
         return newState
+
+
+class Utils:
+    st = []
+
+    def __init__(self, state):
+        self.st = state
+
+    def printBags(self):
+        bags = self.BagsList(len(self.st[0]))
+        outBag = []
+
+        for i in self.st[1]:
+            bag_index = i[1]
+            if bag_index != len(self.st[0]):
+                item = i[0]
+                bags[bag_index].append(item)
+                continue
+            outBag.append(i[0])
+
+        for i in range(len(bags)):
+            print 'Bag {} = {} | Max Vol. = {} , Allocated = {}'.format(i, bags[i], self.st[0][i], sum(bags[i]))
+        print 'Items out of bags: {}'.format(outBag)
+
+    def BagsList(self, size):
+        bags = list()
+        for i in range(0, size):
+            bags.append(list())
+        return bags
