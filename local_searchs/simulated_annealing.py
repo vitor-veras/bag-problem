89997b2238d84bc22c67638efbad54b0fef96039
Unknown <christianomachado10@gmail.com>
2018-03-30 20:33:46 -0300

    Simulate adjust.

--- local_searchs/simulated_annealing.py
+++ local_searchs/simulated_annealing.py
@@ -5,7 +5,7 @@ from local_searchs.heuristic import Heuristic
 from local_searchs.bag_problem import BagProblem
 from math import exp
 
-# Algoritmo Simulated Annealing adaptado de : https://pt.wikipedia.org/wiki/Simulated_annealing
+# Algoritmo Simulated Annealing adaptado de : http://conteudo.icmc.usp.br/pessoas/sandra/G9_t2/annealing.htm
 
 class SimulatedAnnealing(object):
 
@@ -15,41 +15,29 @@ class SimulatedAnnealing(object):
     def __init__(self, iterate=500,
                  maxDis=100,
                  maxSuc=70,
-                 alpha=0.9,
-                 startTemp=300,
-                 initialState= None):
+                 alpha=0.5644577,
+                 startTemp=500,
+                 initialState= None,
+                 finalTemp=0.005):
         self.iterate = iterate
         self.maxDis = maxDis
         self.maxSuc = maxSuc
         self.alpha = alpha
         self.initialState = initialState
         self.startTemp = startTemp
+        self.finalTemp = finalTemp
 
     def simulate(self):
-        i = 0
-        success = sys.maxsize
         currentState = self.initialState
-        t = self.startTemp
-        currentWeight = Heuristic(currentState).getCurrentTotalWeight()
-        totalWeight = Heuristic(currentState).getTotalWeight()
+        T0 = self.startTemp
+        while T0 > self.finalTemp:
+            newState = Neighbor(BagProblem().desalloc(currentState)).generateState()
+            deltaF = Heuristic(currentState).count() - Heuristic(newState).count()
+            if deltaF <= 0:
+                currentState = newState
+            else:
+                if exp(-deltaF/T0) > random.random():
+                    currentState = newState
 
-
-        while not(success == 0) and i < self.iterate and currentWeight != totalWeight:
-            j = 0
-            success = 0
-            while success <= self.maxSuc and j < self.maxDis:
-                f1 = Heuristic(currentState).count()
-                newState = Neighbor(BagProblem().desalloc(currentState)).generateState()
-
-                f2 = Heuristic(newState).count()
-                deltaF = f1 - f2
-                if not t == 0.0:
-                    if (deltaF <= 0) or (exp(-deltaF/t) > random.random()):
-                        currentState = newState
-                        success += 1
-
-                j += 1
-            t = self.alpha * t
-            i += 1
-
+            T0 = T0 * self.alpha
         return currentState
